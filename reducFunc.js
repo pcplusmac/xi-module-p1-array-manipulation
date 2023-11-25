8818e5ed7cba7d50dbcb48935caad6ca3308e2d6
pc-sam <samurai.h@yahoo.com>
2023-11-25 22:21:45 +0800

    add js file for reduce method

---
+++ reducFunc.js
@@ -0,0 +1,36 @@
+let books = [
+    {
+        title: 'book1',
+        price: 16.00,
+        inventory: 5
+    },
+    {
+        title: 'book2',
+        price: 13.30,
+        inventory: 15
+    },
+    {
+        title: 'book3',
+        price: 9.00,
+        inventory: 6
+    },
+    {
+        title: 'book4',
+        price: 26.00,
+        inventory: 36
+    },
+    {
+        title: 'book5',
+        price: 11.00,
+        inventory: 25
+    },
+    {
+        title: 'book6',
+        price: 96.00,
+        inventory: 3
+    },
+    {
+        title: 'book7',
+        price: 6.50,
+        inventory: 85
+    }] 
